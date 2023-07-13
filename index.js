f0e8784ad50103b95c8c2b6a36f97bcc04e4aee5
sfilata <shubaiqiao@126.com>
2023-07-13 16:58:13 +0800

    feat: Add the vue and typescript rules.

--- index.js
+++ index.js
@@ -1,70 +1,142 @@
-const parser = require('@typescript-eslint/parser');
-
 module.exports = {
-  root: true,
+  env: {
+    browser: true,
+    es2021: true
+  },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended'
+    'plugin:@typescript-eslint/recommended',
+    'plugin:vue/vue3-essential'
   ],
-  plugins: ['@typescript-eslint'],
-  parser: '@typescript-eslint/parser',
   parserOptions: {
-    sourceType: "module",
-    tsconfigRootDir: process.cwd(),
-    ecmaVersion: 2022,
-    ecmaFeatures: {
-      jsx: true,
-    },
+    ecmaVersion: 'latest',
+    parser: '@typescript-eslint/parser',
     sourceType: 'module',
     project: true,
+    extraFileExtensions: ['.vue']
   },
-  globals: {
-    document: 'readonly',
-    navigator: 'readonly',
-    window: 'readonly',
-  },
-  // plugins: [
-  //   'import',
-  //   'n',
-  //   'promise',
-  // ],
-  settings: {
-    'import/resolver': {
-      node: { extensions: ['.js', '.jsx', '.mjs', '.ts', '.tsx', '.d.ts'] },
-    },
-  },
-  env: {
-    es2021: true,
-    node: true
-  },
+  plugins: [
+    '@typescript-eslint',
+    'vue'
+  ],
   rules: {
-    "semi": [2, "always"],
-    "quotes": [2, "single"],
-    "@typescript-eslint/naming-convention": "off",
-    "@typescript-eslint/semi": "warn",
-    "curly": "warn",
-    "eqeqeq": "warn",
-    "no-throw-literal": "warn",
-    "max-len": [
-      "error",
+    indent: ['error', 2],
+    quotes: ['error', 'single'],
+    semi: ['error', 'always'],
+    'quote-props': ['error', 'as-needed'],
+    'linebreak-style': ['error', 'unix'],
+    'array-element-newline': ['error', 'consistent'],
+    'array-bracket-newline': ['error', 'consistent'],
+    'comma-dangle': ['error', {
+      arrays: 'never',
+      objects: 'never',
+      imports: 'never',
+      exports: 'never',
+      functions: 'never'
+    }],
+    'comma-spacing': ['error', { before: false, after: true }],
+    'block-spacing': 'error',
+    'array-bracket-spacing': ['error', 'never'],
+    // Vue eslint
+    'vue/max-attributes-per-line': 'off',
+    'vue/no-v-html': 'off',
+    'vue/require-prop-types': 'off',
+    'vue/require-default-prop': 'off',
+    'vue/multi-word-component-names': 'off',
+    'vue/prefer-import-from-vue': 'off',
+    'vue/no-v-text-v-html-on-component': 'off',
+    'vue/no-dupe-keys': 'off',
+
+    // reactivity transform
+    'vue/no-setup-props-destructure': 'off',
+
+    'vue/component-tags-order': ['error', {
+      order: ['template', 'script', 'style']
+    }],
+    'vue/block-tag-newline': ['error', {
+      singleline: 'always',
+      multiline: 'always'
+    }],
+    'vue/component-name-in-template-casing': ['error', 'PascalCase'],
+    'vue/component-options-name-casing': ['error', 'PascalCase'],
+    'vue/custom-event-name-casing': ['error', 'camelCase'],
+    'vue/define-macros-order': ['error', {
+      order: ['defineOptions', 'defineProps', 'defineEmits', 'defineSlots']
+    }],
+    'vue/html-comment-content-spacing': ['error', 'always', {
+      exceptions: ['-']
+    }],
+    'vue/no-restricted-v-bind': ['error', '/^v-/'],
+    'vue/no-useless-v-bind': 'error',
+    'vue/no-unused-refs': 'error',
+    'vue/padding-line-between-blocks': ['error', 'always'],
+    'vue/prefer-separate-static-class': 'error',
+
+    // extensions
+    'vue/array-bracket-spacing': ['error', 'never'],
+    'vue/arrow-spacing': ['error', { before: true, after: true }],
+    'vue/block-spacing': ['error', 'always'],
+    'vue/brace-style': ['error', 'stroustrup', { allowSingleLine: true }],
+    'vue/comma-spacing': ['error', { before: false, after: true }],
+    'vue/comma-style': ['error', 'last'],
+    'vue/dot-location': ['error', 'property'],
+    'vue/dot-notation': ['error', { allowKeywords: true }],
+    'vue/eqeqeq': ['error', 'smart'],
+    // 'vue/func-call-spacing': ['off', 'never'],
+    'vue/key-spacing': ['error', { beforeColon: false, afterColon: true }],
+    'vue/keyword-spacing': ['error', { before: true, after: true }],
+    'vue/no-constant-condition': 'warn',
+    'vue/no-empty-pattern': 'error',
+    'vue/no-extra-parens': ['error', 'functions'],
+    'vue/no-irregular-whitespace': 'error',
+    'vue/no-loss-of-precision': 'error',
+    'vue/no-restricted-syntax': [
+      'error',
+      'DebuggerStatement',
+      'LabeledStatement',
+      'WithStatement'
+    ],
+    'vue/no-sparse-arrays': 'error',
+    'vue/object-curly-newline': ['error', { multiline: true, consistent: true }],
+    'vue/object-curly-spacing': ['error', 'always'],
+    'vue/object-property-newline': ['error', { allowMultiplePropertiesPerLine: true }],
+    'vue/object-shorthand': [
+      'error',
+      'always',
       {
-        "ignoreComments": true,
-        "code": 120
+        ignoreConstructors: false,
+        avoidQuotes: true
       }
     ],
-    "object-curly-spacing": [
-      "error",
-      "always"
-    ],
-    "@typescript-eslint/no-unsafe-member-access": "warn",
-    "quote-props": ["error", "as-needed"],
-    "no-async-promise-executor": "off"
-  },
-  ignorePatterns: [
-    "out",
-    "dist",
-    "assets",
-    "bin",
-    "config"
-  ]
-}
+    'vue/operator-linebreak': ['error', 'before'],
+    'vue/prefer-template': 'error',
+    'vue/quote-props': ['error', 'consistent-as-needed'],
+    'vue/space-in-parens': ['error', 'never'],
+    'vue/space-infix-ops': 'error',
+    'vue/space-unary-ops': ['error', { words: true, nonwords: false }],
+    'vue/template-curly-spacing': 'error',
+
+    // typescript
+    'no-throw-literal': 'off',
+    '@typescript-eslint/no-throw-literal': 'error',
+    'no-implied-eval': 'off',
+    '@typescript-eslint/no-implied-eval': 'error',
+    'dot-notation': 'off',
+    '@typescript-eslint/dot-notation': ['error', { allowKeywords: true }],
+    '@typescript-eslint/no-floating-promises': 'error',
+    '@typescript-eslint/no-misused-promises': 'error',
+    '@typescript-eslint/await-thenable': 'error',
+    '@typescript-eslint/no-for-in-array': 'error',
+    '@typescript-eslint/no-unnecessary-type-assertion': 'error',
+    '@typescript-eslint/no-unsafe-argument': 'error',
+    '@typescript-eslint/no-unsafe-assignment': 'error',
+    '@typescript-eslint/no-unsafe-call': 'error',
+    '@typescript-eslint/no-unsafe-member-access': 'error',
+    '@typescript-eslint/no-unsafe-return': 'error',
+    'require-await': 'off',
+    '@typescript-eslint/require-await': 'error',
+    '@typescript-eslint/restrict-plus-operands': 'error',
+    '@typescript-eslint/restrict-template-expressions': 'error',
+    '@typescript-eslint/unbound-method': 'error'
+  }
+};
